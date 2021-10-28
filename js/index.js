7c44b78aecefe6571e5a279a3b7d3c2e0e78388d
Lara-Vitoria <larinhavitoria12@gmail.com>
2021-10-28 18:47:16 -0300

    cadastro funcionando?

--- js/index.js
+++ js/index.js
@@ -7,3 +7,47 @@ function closeNav() {
     document.getElementById("mySidebar").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
 }
+
+
+
+document.getElementById("UsuarioTxt").onclick = function() {
+    var userName = document.getElementById("UsuarioTxt").value;
+    var senha = document.getElementById("SenhaTxt").value;
+    facCadastro('/client/post',userName,senha);
+};
+
+function fazCadastro(url, userName, senha) {
+
+    if (window.XMLHttpRequest) { // Mozilla, Safari, ...
+      httpRequest = new XMLHttpRequest();
+    } else if (window.ActiveXObject) { // IE
+      try {
+        httpRequest = new ActiveXObject("Msxml2.XMLHTTP");
+      }
+      catch (e) {
+        try {
+          httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        catch (e) {}
+      }
+    }
+
+    if (!httpRequest) {
+      alert('nao criou instancia XMLHTTP');
+      return false;
+    }
+    httpRequest.onreadystatechange = alertContents;
+    httpRequest.open('POST', url);
+    httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    httpRequest.send('userName=' + encodeURIComponent(userName) + 'senha=' + encodeURIComponent(senha));
+}
+
+function alertContents() {
+    if (httpRequest.readyState === 4) {
+      if (httpRequest.status === 200) {
+        alert(httpRequest.responseText);
+      } else {
+        alert('Problema com o request.');
+      }
+    }
+}
